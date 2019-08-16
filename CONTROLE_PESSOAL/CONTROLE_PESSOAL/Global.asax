5467ae4166e862e51b94b19434a9b1bd1b189ac8
Wesley Gonçalves Luciano <wesleylg01@gmail.com>
2019-08-15 23:08:05 -0300

    Criação projeto asp .net

---
+++ CONTROLE_PESSOAL/CONTROLE_PESSOAL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CONTROLE_PESSOAL.MvcApplication" Language="C#" %>
---
+++ CONTROLE_PESSOAL/CONTROLE_PESSOAL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CONTROLE_PESSOAL.Startup))]
+namespace CONTROLE_PESSOAL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
